4921b676bdaf93a36eed00ee1b604058a7100fb0
Kamil Kolanowski <tanssive@yahoo.com>
2024-06-04 23:27:11 +0200

    cleansed main script, add DataOperation class

--- scripts/main.py
+++ scripts/main.py
@@ -1,21 +1,22 @@
 from typing import List, Any
 from APIConnector import APIConnector
 import polars as pl
-import pprint
 import os
 
-if __name__ == "__main__":
-    api_conn = APIConnector(os.getenv('API_URL'))
 
-    def get_characters():
-        characters_source: list[Any] = api_conn.get_paginated_data('character')
+class DataOperations:
+    def __init__(self):
+        self.api_conn = APIConnector(os.getenv('API_URL'))
+
+    def get_characters(self):
+        characters_source: list[Any] = self.api_conn.get_paginated_data('character')
         characters_data = pl.DataFrame(characters_source)
 
         characters_cols_list = ["id", "name", "status", "species", "gender", "location", "episode"]
         characters_selected = characters_data.select(characters_cols_list)
         characters = (characters_selected
                       .with_columns(characters_selected['location'].map_elements(lambda x: x['name']
-                                                                                    if x is not None else None))
+        if x is not None else None))
                       .explode('episode')
                       .with_columns(pl.col('episode')
                                     .str
@@ -25,21 +26,29 @@ if __name__ == "__main__":
 
         return characters
 
-    def get_episodes():
-        episodes_source: list[Any] = api_conn.get_paginated_data('episode')
+    def get_locations(self):
+        locations_source: list[Any] = self.api_conn.get_paginated_data('location')
+        locations_data = pl.DataFrame(locations_source)
+
+        return locations_data
+
+    def get_episodes(self):
+        episodes_source: list[Any] = self.api_conn.get_paginated_data('episode')
         episodes_data = pl.DataFrame(episodes_source)
 
         return episodes_data
 
+    @staticmethod
     def count_appearances_in_episodes(characters):
         characters_in_episodes = (characters
-                          .groupby('name')
-                          .count()
-                          .sort('count')
-                          )
+                                  .groupby('name')
+                                  .count()
+                                  .sort('count')
+                                  )
 
         return characters_in_episodes
 
+    @staticmethod
     def get_joined_characters_episodes_locations(characters, episodes, location):
         return (characters
                 .join(episodes,
@@ -62,12 +71,21 @@ if __name__ == "__main__":
                         )
                 )
 
-    def get_locations():
-        locations_source: list[Any] = api_conn.get_paginated_data('location')
-        locations_data = pl.DataFrame(locations_source)
+    def write_results(self):
+        characters_episodes_locations = self.get_joined_characters_episodes_locations(self.get_characters(),
+                                                                                 self.get_episodes(),
+                                                                                 self.get_locations())
 
-        return locations_data
+        characters_episodes_locations.write_csv('../results/characters_episodes_locations.csv')
+
+        appearances_in_episodes = self.count_appearances_in_episodes(self.get_characters())
+        appearances_in_episodes.write_csv('../results/appearances_in_episodes.csv')
 
 
-    result = get_joined_characters_episodes_locations(get_characters(), get_episodes(), get_locations())
-    result.write_csv('../results/characters_episodes_locations.csv')
+if __name__ == "__main__":
+    do = DataOperations()
+    do.write_results()
+
+
+
+
